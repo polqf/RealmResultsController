63f5b73a2d63816d1e73ef1477de2ad92806c3af
Pol Quintana <polkintana@gmail.com>
2015-08-06 15:53:02 +0200

    RealmNotification tests

---
+++ Example/RealmResultsControllerTests/RealmNotificationTests.swift
@@ -0,0 +1,46 @@
+//
+//  RealmNotificationTests.swift
+//  RealmResultsController
+//
+//  Created by Pol Quintana on 6/8/15.
+//  Copyright © 2015 Redbooth. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+import RealmSwift
+
+@testable import RealmResultsController
+
+class RealmNotificationSpec: QuickSpec {
+    override func spec() {
+        var realm: Realm!
+        beforeSuite {
+            RealmTestHelper.loadRealm()
+            realm = try! Realm()
+        }
+        describe("loggerForRealm(realm:)") {
+            var createdLogger: RealmLogger!
+            context("Create a logger") {
+                beforeEach {
+                    createdLogger = RealmNotification.loggerForRealm(realm)
+                }
+                it("Should have stored the logger in its shared instance") {
+                    expect(RealmNotification.sharedInstance.loggers.count).to(equal(1))
+                    expect(RealmNotification.sharedInstance.loggers.first!.notificationToken).to(equal(createdLogger.notificationToken))
+                }
+            }
+            context("Retrieve a created logger") {
+                var retrievedLogger: RealmLogger!
+                beforeEach {
+                    retrievedLogger = RealmNotification.loggerForRealm(realm)
+                }
+                it("Should have retrieve the logger stored in its shared instance") {
+                    expect(RealmNotification.sharedInstance.loggers.count).to(equal(1))
+                    expect(retrievedLogger.notificationToken).to(equal(createdLogger.notificationToken))
+                }
+            }
+        }
+    }
+}
