7a4881516da89ea9e93a5032e766f10502e56e2e
isaacroldan <isaac.roldan@gmail.com>
2015-08-06 15:07:59 +0200

    added tests to Section class

--- Example/RealmResultsControllerTests/RealmResultsCacheTests.swift
+++ Example/RealmResultsControllerTests/RealmResultsCacheTests.swift
@@ -13,36 +13,148 @@ import RealmSwift
 
 @testable import RealmResultsController
 
-class TableOfContentsSpec: QuickSpec {
+
+func openRealm() {
     
-    func openRealm() {
-        
-        let defaultRealmPath = Realm.defaultPath
-        let bundleReamPath = NSBundle.mainBundle().resourcePath?.stringByAppendingPathComponent("test.realm")
-        
-        if !NSFileManager.defaultManager().fileExistsAtPath(defaultRealmPath) {
-            try! NSFileManager.defaultManager().copyItemAtPath(bundleReamPath!, toPath: defaultRealmPath)
-        }
+    let defaultRealmPath = Realm.defaultPath
+    let bundleReamPath = NSBundle.mainBundle().resourcePath?.stringByAppendingPathComponent("test.realm")
+    
+    if !NSFileManager.defaultManager().fileExistsAtPath(defaultRealmPath) {
+        try! NSFileManager.defaultManager().copyItemAtPath(bundleReamPath!, toPath: defaultRealmPath)
     }
-    
+}
+
+
+class SectionSpec: QuickSpec {
     
     override func spec() {
         
-        openRealm()
-        let realm = try! Realm()
+//        openRealm()
         
-        describe("the 'Documentation' directory") {
+        var sortDescriptors: [NSSortDescriptor]!
+        var section: Section<Task>!
+        var openTask: Task!
+        var resolvedTask: Task!
+        
+        beforeSuite {
+            sortDescriptors = [NSSortDescriptor(key: "name", ascending: false)]
+            section = Section<Task>(keyPath: "keyPath", sortDescriptors: sortDescriptors)
+            openTask = Task()
+            openTask.id = 1500
+            openTask.name = "aatest"
+            openTask.resolved = false
+            
+            resolvedTask = Task()
+            resolvedTask.id = 1501
+            resolvedTask.name = "bbtest"
+            resolvedTask.resolved = false
+        }
+        
+        describe("create a Section object") {
             it("has everything you need to get started") {
-                let total = realm.objects(Task.self)
-                expect(total.count).to(equal(1001))
+                expect(section.keyPath).to(equal("keyPath"))
+                expect(section.sortDescriptors).to(equal(sortDescriptors))
+            }
+        }
+        
+        describe("insertSorted") {
+            
+            var index: Int!
+            context("when the section is empty") {
+                it ("beforeAll") {
+                    index = section.insertSorted(openTask)
+                }
+                it("a has one item") {
+                    expect(section.objects.count).to(equal(1))
+                }
+                it("item has index 0") {
+                    expect(index).to(equal(0))
+                }
             }
             
-            context("if it doesn't have what you're looking for") {
-                it("needs to be updated") {
-//                    let you = You(awesome: true)
-//                    expect{you.submittedAnIssue}.toEventually(beTruthy())
+            context("when the section is not empty") {
+                it("beforeAll") {
+                    index = section.insertSorted(resolvedTask)
+                }
+                it("has two items") {
+                    expect(section.objects.count).to(equal(2))
+                }
+                it("has index 0") { // beacuse of the sortDescriptor
+                    expect(index).to(equal(0))
                 }
             }
         }
+        
+        describe("delete") {
+            var originalIndex: Int!
+            var index: Int!
+            context("when the object exists in section") {
+                beforeEach {
+                    section = Section<Task>(keyPath: "keyPath", sortDescriptors: sortDescriptors)
+                    section.insertSorted(resolvedTask)
+                    originalIndex = section.insertSorted(openTask)
+                    index = section.delete(openTask)
+                }
+                it("removes it from array") {
+                    expect(section.objects.containsObject(openTask)).to(beFalsy())
+                }
+                it("returns the index of the deleted object") {
+                    expect(index).to(equal(originalIndex))
+                }
+            }
+            
+            context("the object does not exists in section") {
+                var anotherTask: Task!
+                beforeEach {
+                    section = Section<Task>(keyPath: "keyPath", sortDescriptors: sortDescriptors)
+                    section.insertSorted(resolvedTask)
+                    section.insertSorted(openTask)
+                    anotherTask = Task()
+                    index = section.delete(anotherTask)
+                }
+                it("returns index -1") {
+                    expect(index).to(equal(-1))
+                }
+            }
+        }
+        
+        describe("deleteOutdatedObject") {
+            var originalIndex: Int!
+            var index: Int!
+            context("when the object exists in section") {
+                beforeEach {
+                    section = Section<Task>(keyPath: "keyPath", sortDescriptors: sortDescriptors)
+                    section.insertSorted(resolvedTask)
+                    originalIndex = section.insertSorted(openTask)
+                    index = section.delete(openTask)
+                }
+                it("removes it from array") {
+                    expect(section.objects.containsObject(openTask)).to(beFalsy())
+                }
+                it("returns the index of the deleted object") {
+                    expect(index).to(equal(originalIndex))
+                }
+            }
+            var anotherTask: Task!
+            context("the object does not exists in section") {
+                beforeEach {
+                    section = Section<Task>(keyPath: "keyPath", sortDescriptors: sortDescriptors)
+                    section.insertSorted(resolvedTask)
+                    section.insertSorted(openTask)
+                    anotherTask = Task()
+                    index = section.delete(anotherTask)
+                }
+                it("returns index -1") {
+                    expect(index).to(equal(-1))
+                }
+            }
+        }
+        
     }
 }
+
+
+
+
+
+
