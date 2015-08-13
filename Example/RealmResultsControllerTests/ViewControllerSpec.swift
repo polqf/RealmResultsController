b630d7c68cb9f82dd8250bd9ddd01211cb643cf3
isaacroldan <isaac.roldan@gmail.com>
2015-08-13 13:37:19 +0200

    add VC tests

---
+++ Example/RealmResultsControllerTests/ViewControllerSpec.swift
@@ -0,0 +1,31 @@
+//
+//  ViewControllerSpec.swift
+//  RealmResultsController
+//
+//  Created by Isaac Roldan on 13/8/15.
+//  Copyright © 2015 Redbooth. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+import RealmSwift
+
+@testable import RealmResultsController
+
+
+/// Note: Specs created only to reach 100% coverage. 
+/// Until Xcode lets you ignore View files in the coverage
+class ViewControllerSpec: QuickSpec {
+    
+    override func spec() {
+        var VC: ViewController!
+        
+        beforeEach {
+            VC = ViewController(coder: NSCoder())
+        }
+        it("it is not nil") {
+            expect(VC).toNot(beNil())
+        }
+    }
+}
