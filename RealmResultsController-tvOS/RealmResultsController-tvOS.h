9a36153488147b53f5d061a14465c1b1a6a7f7b6
Maria Bernis <mariabernis.clavijo@gmail.com>
2016-05-03 14:05:21 +0200

    Added tvOS target

---
+++ RealmResultsController-tvOS/RealmResultsController-tvOS.h
@@ -0,0 +1,19 @@
+//
+//  RealmResultsController-tvOS.h
+//  RealmResultsController-tvOS
+//
+//  Created by Maria Bernis on 03/05/16.
+//  Copyright © 2016 Redbooth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RealmResultsController-tvOS.
+FOUNDATION_EXPORT double RealmResultsController_tvOSVersionNumber;
+
+//! Project version string for RealmResultsController-tvOS.
+FOUNDATION_EXPORT const unsigned char RealmResultsController_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealmResultsController_tvOS/PublicHeader.h>
+
+
