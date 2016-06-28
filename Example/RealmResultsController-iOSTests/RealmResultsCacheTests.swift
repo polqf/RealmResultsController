f4429373a84ffd5128e0b0c1d782181768116f75
Jared Allen <jared@redcact.us>
2016-06-27 18:22:47 -0700

    filter initial objects
    Use array filter instead of predicate to limit initial set of tasks to be contained in one section using "resolved" sectionKeyPath.

--- Example/RealmResultsController-iOSTests/RealmResultsCacheTests.swift
+++ Example/RealmResultsController-iOSTests/RealmResultsCacheTests.swift
@@ -69,14 +69,14 @@ class CacheSpec: QuickSpec {
         
         func initWithKeypath(onlyResolved: Bool = false) {
             predicate = NSPredicate(format: "id < %d", 50)
-
-            if onlyResolved {
-                predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [predicate, NSPredicate(format: "resolved == true")])
-            }
-
             sortDescriptors = [SortDescriptor(property: "name", ascending: true)]
             request = RealmRequest<Task>(predicate: predicate, realm: realm, sortDescriptors: sortDescriptors)
             initialObjects = request.execute().toArray().sort { $0.name < $1.name }
+
+            if onlyResolved {
+                initialObjects = initialObjects.filter { $0.resolved }
+            }
+
             resolvedTasks = initialObjects.filter { $0.resolved }
             notResolvedTasks = initialObjects.filter { !$0.resolved }
             cache = RealmResultsCache<Task>(request: request, sectionKeyPath: "resolved")
